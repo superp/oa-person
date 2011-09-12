fac9ece7fc96a6c110cd4fc52cc3f03374f0e01b
superp <superp1987@gmail.com>
2011-09-12 18:44:33 +0300

    fix

--- lib/oa_person/streams/vkontakte.rb
+++ lib/oa_person/streams/vkontakte.rb
@@ -12,7 +12,8 @@ module OaPerson
       #    :attachment => 'link'
       #    
       def publish(options = {})
-        client.wall_post(options)
+        options = {:wall_id => uid}.merge(options)
+        client.wall.savePost(options)
       end
       
       def image_by_type(type)
@@ -20,16 +21,20 @@ module OaPerson
         image
       end
       
-      def profile_image_source
-        client.getProfiles(:uids => uid, :fields => 'photo_big,photo,photo_medium,photo_rec').inspect
+      def profile_image_source(options)
+        #client.getProfiles(:uids => uid, :fields => 'photo_big,photo,photo_medium,photo_rec').inspect
+        options = {:uid => uid}.merge(options)
+        client.photos.get(options)
       end
       
       protected
       
         def connect
           begin
-            #OaPerson::VkClient.new(uid, access_token, 2457517, 'WUJhMeMDy2opiqSqJ4K3')
-            app = ::VK::Serverside.new(:app_id => 2457517, :app_secret => 'WUJhMeMDy2opiqSqJ4K3')
+            #OaPerson::VkClient.new(uid, access_token, your_id, 'your_secret')
+            
+            #TODO get this values from omniauth config
+            app = ::VK::Serverside.new(:app_id => 'your_id', :app_secret => 'your_secret')
             app.access_token = access_token
             app
           rescue Exception => e
